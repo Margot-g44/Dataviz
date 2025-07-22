2455574328a359c7ba64c57ac15bd7c3ec2ae91b
Elouan <elouangauriaud@gmail.com>
2025-07-22 14:03:09 +0200

    Rajout d'une fonction asynchrone pour récupérer l'API et affichage sur HTML

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function checkTime(i) {
 }
  
 startTime();
+  
+async function getWeather() {
+  const API_KEY = "f726ef78e6b1a9b2d37393a7d1aef3db";
+  const CITY = document.getElementById("city").value;
+  // const URL = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(CITY)}&appid=${API_KEY}&units=metric&lang=fr`; API pour la météo en temps réel
+  const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(CITY)}&appid=${API_KEY}&units=metric&lang=fr`; // API pour une durée de 5 jours avec un affichage toutes les 3h
+
+  try {
+    const RESPONSE = await fetch(URL);
+    const DATA = await RESPONSE.json();
+
+      if (RESPONSE.ok) {
+        document.getElementById("resultat").textContent = JSON.stringify(DATA, null, 2);
+      } else {
+        document.getElementById("resultat").textContent = `Erreur : ${DATA.message}`;
+      }
+    } catch (error) {
+    document.getElementById("resultat").textContent = `Erreur réseau : ${error}`;
+  }
+}
